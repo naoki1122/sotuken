6f73c77dde69b903ce7c1123ac5a3e18dd4cbc94
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-11-16 16:13:39 +0900

    一応動きます

--- addteacher.php
+++ addteacher.php
@@ -8,22 +8,31 @@ $host = 'localhost';
 // MySQLのDSN文字列
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 
-$stm = $pdo->prepare($sql);
-$stm->execute();
-$result = $stm->fetchAll(PDO::FETCH_ASSOC);
 try {
     $pdo = new PDO($dsn, $user, $password);
     // プリペアドステートメントのエミュレーションを無効にする
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     // 例外がスローされる設定にする
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if(isset($_POST['name'])){
-        
-    }
-} catch (Exception $e) {
+    
+}catch (Exception $e) {
     echo '<span class="error">エラーがありました。</span><br>';
     echo $e->getMessage();
   }
+
+  if(isset($_POST['name'],$_POST['no'],$_POST['password'],$_POST['authority'])){
+      $name = $_POST['name'];
+      $no = $_POST['no'];
+      $password = $_POST['password'];
+      $authority = $_POST['authority'];
+      
+    $stmt = $pdo->prepare("insert  into management.teacher(名前,教員番号,パスワード,権限) VALUES (?,?,?,?)");
+    $stmt->execute([$name, $no,$password,$authority]);
+  echo '登録完了';
+}
+else{
+    $word = "NO";
+}
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +48,7 @@ try {
 <!--入力フォーム-->
 <form id="addteacher" action="" method="post">
     ・<input type="text" name="name" required autofocus placeholder="名前"><br>
-    ・<input type="text" name="number" required placeholder="教員番号"><br>
+    ・<input type="text" name="no" required placeholder="教員番号"><br>
     ・<input type="password" name="password" required placeholder="パスワード"><br>
     ・<select name="authority" required>
         <option value="" selected>権限を選択してください</option>
@@ -53,4 +62,3 @@ try {
 <small>© チームコリジョン2019</small>
 </body>
 </html>
-
