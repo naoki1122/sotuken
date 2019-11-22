b5d0eade0ff256d37bb92cd284527e1c6adce6b4
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-11-22 12:55:30 +0900

    さわっている

--- Web/login.php
+++ Web/login.php
@@ -10,22 +10,22 @@ session_start();
 //DB内でPOSTされたメールアドレスを検索
 try {
   $pdo = new PDO($dsn, $user, $password);
-  $stmt = $pdo->prepare('select * from teacher where 名前 = ?');
-  $stmt->execute([$_POST['名前']]);
+  $stmt = $pdo->prepare('select * from teacher where 教員番号 = ?');
+  $stmt->execute([$_POST['教員番号']]);
   $row = $stmt->fetch(PDO::FETCH_ASSOC);
 } catch (\Exception $e) {
   echo $e->getMessage() . PHP_EOL;
 }
 var_dump($row['パスワード']);
 //emailがDB内に存在しているか確認
-if (!isset($row['名前'])) {
+if (!isset($row['教員番号'])) {
   echo 'メールアドレス又はパスワードが間違っています。';
   return ;
 }
 //パスワード確認後sessionにメールアドレスを渡す
 if ($_POST['パスワード'] == $row['パスワード']) {
   session_regenerate_id(true); //session_idを新しく生成し、置き換える
-  $_SESSION['名前'] = $row['名前'];
+  $_SESSION['教員番号'] = $row['教員番号'];
   echo 'ログインしました';
 	exit;
 } else {
