d8538397469ad023d8c73c9ae326d1f946ff6c4f
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-11-25 14:21:44 +0900

    const関数に変更

--- Web/localhost_config.php
+++ Web/localhost_config.php
@@ -1,6 +1,11 @@
 <?php
 
 //ini_set('display_errors', 1);
+
+// define('DSN', 'mysql:host=localhost;dbname=management');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+
 const DSN = "mysql:host=localhost;dbname=management";
 const DB_USER = "root";
 const DB_PASS = "";
