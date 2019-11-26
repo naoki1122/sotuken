a587453a0887039801eff583fea3be35358195c0
takahashi-0124 <x18n050@chiba.ac.jp>
2019-11-26 10:01:27 +0900

    a

--- Web/teacher_list.php
+++ Web/teacher_list.php
@@ -12,7 +12,9 @@ require_once("localhost_config.php");
 </head>
 
 <body>
-<H1>教員一覧</H1><br>
+<!--戻るのリンク-->
+<a href="teacher_list.html">戻る</a><br>
+<H1 align="left">教員一覧</H1><br>
     <?php
 //require_once('main_config.php');
 require_once('localhost_config.php');
