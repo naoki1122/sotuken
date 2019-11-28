4a127d8b31a88c5f8b801a2c326f6732108bd39e
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-11-28 16:58:09 +0900

    久保へ お仕事頑張っってください

--- Web/android_login.php
+++ Web/android_login.php
@@ -1,33 +1,33 @@
 <?php
-//sotukenサーバー用のDB情報
-//require_once("server_config.php");
-//ローカル用のサーバー情報
 require_once("localhost_config.php");
+$word = $_POST['word']; 
+list($id,$name) = explode(",",$word);
 
-session_start();
+echo $id;
+echo $name;
 
-//DB内でPOSTされたメールアドレスを検索
 try {
-  $pdo = new PDO(DSN,DB_USER,DB_PASS);
-  $stmt = $pdo->prepare('select * from student where 名前 = ?');
-  $stmt->execute([$_POST['名前']]);
-  $row = $stmt->fetch(PDO::FETCH_ASSOC);
-} catch (\Exception $e) {
-  echo $e->getMessage() . PHP_EOL;
-}
-//名前がDB内に存在しているか確認
-if (!isset($row['名前'])) {
-  echo '名前が間違っています。';
-  return ;
-}
-//パスワード確認後sessionにメールアドレスを渡す　password_verify
-if ($_POST['パスワード']==$row['パスワード']) {
-  session_regenerate_id(true); //session_idを新しく生成し、置き換える
-  $_SESSION['名前'] = $row['名前'];
-  echo 'ログインしました';
-	exit;
-} else {
-  echo 'メールアドレス又はパスワードが間違っています。';
-  return false;
-}
+    $pdo = new PDO(DSN,DB_USER,DB_PASS);
+    $stmt = $pdo->prepare('select * from student where 名前 = ?');
+    $stmt->execute($name);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  } catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+  }
+  //名前がDB内に存在しているか確認
+  if (!isset($row['名前'])) {
+    echo '名前が間違っています。';
+    return ;
+  }
+  //パスワード確認後sessionにメールアドレスを渡す　password_verify
+  if ($id==$row['id']) {
+    session_regenerate_id(true); //session_idを新しく生成し、置き換える
+    $_SESSION['名前'] = $row['名前'];
+    echo 'ログインしました';
+      exit;
+  } else {
+    echo 'メールアドレス又はパスワードが間違っています。';
+    return false;
+  }
+
 ?>
