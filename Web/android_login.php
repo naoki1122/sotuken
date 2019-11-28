80d16942a12a6e4428646a7c58c7d76d4a95ed3c
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-11-28 16:41:06 +0900

    アンドロイド　ログイン処理

---
+++ Web/android_login.php
@@ -0,0 +1,33 @@
+<?php
+//sotukenサーバー用のDB情報
+//require_once("server_config.php");
+//ローカル用のサーバー情報
+require_once("localhost_config.php");
+
+session_start();
+
+//DB内でPOSTされたメールアドレスを検索
+try {
+  $pdo = new PDO(DSN,DB_USER,DB_PASS);
+  $stmt = $pdo->prepare('select * from student where 名前 = ?');
+  $stmt->execute([$_POST['名前']]);
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+} catch (\Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
+//名前がDB内に存在しているか確認
+if (!isset($row['名前'])) {
+  echo '名前が間違っています。';
+  return ;
+}
+//パスワード確認後sessionにメールアドレスを渡す　password_verify
+if ($_POST['パスワード']==$row['パスワード']) {
+  session_regenerate_id(true); //session_idを新しく生成し、置き換える
+  $_SESSION['名前'] = $row['名前'];
+  echo 'ログインしました';
+	exit;
+} else {
+  echo 'メールアドレス又はパスワードが間違っています。';
+  return false;
+}
+?>
