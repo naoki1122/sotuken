d5253a3bad20a48455d409ec0b3ccc1fa4eb41b7
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-12-02 02:38:06 +0900

    手直し中

--- Web/student_update.php
+++ Web/student_update.php
@@ -1,102 +1,80 @@
-<!--
+<?php
 //sotukenサーバー用のDB情報
-//require_once("server_config.php");
+//require_once "server_config.php";
 //ローカル用のサーバー情報
-// require_once("server_config.php");
-// $gobackURL = "teacher_update.php";
-// if(isset($_POST['word'])){
-//     $word = $_POST['word'];
-//   }
-//   else{
-//       $word = "";
-//   }
+require_once "localhost_config.php";
+require_once "lib.php";
 
-// try {
-//     $pdo = new PDO(DSN, DB_USER, DB_PASS);
-//     // プリペアドステートメントのエミュレーションを無効にする
-//     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-//     // 例外がスローされる設定にする
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-// }catch (Exception $e) {
-//     echo '<span class="error">エラーがありました。</span><br>';
-//     echo $e->getMessage();
-//   }
+$gobackURL = "student_update.php";
+if(isset($_POST['word'])){
+    $word = $_POST['word'];
+  }
+  else{
+      $word = "";
+  }
 
-//   $name = "";
-//   $no = "";
-//   $password = "";
-//   $authority = "";
-//   $sql = "";
+$pdo = dbcon();
 
-//   if(isset($_POST['検索'])){
-//     if(isset($_POST['word']) && $_POST['mode'] == "名前"){
-//         $mode = "名前";
-//         $word = $_POST['word'];
-//         setcookie("word",$_POST['word']);
-//         setcookie("mode",$_POST['mode']);
-//         $sql = "select * from management.teacher where 名前 = ?";
-//         var_dump($sql);
-//     }else if(isset($_POST['word']) && $_POST['mode'] == "教員番号"){
-//         $mode = "教員番号";
-//         $word = $_POST['word'];
-//         setcookie("word",$_POST['word']);
-//         setcookie("mode",$_POST['mode']);
-//         $sql = "select * from management.teacher where 教員番号 = ?";
-//         var_dump($sql);
-//     }else{
-//         header("Location:{$gobackURL}");
-//     }
+  $name="";$name2="";$no="";
+  $class="";$class2="";$password="";$tel="";
+  $train1="";$train2="";$train3="";$mail="";$subject="";
+  
+  $authority;
+  $sql;
 
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->execute([$word]);
-//     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//     foreach ($result as $row){
-//        $name  = $row["名前"];
-//        $no = $row["教員番号"];
-//        $password = $row["パスワード"];
-//        $authority = $row["権限"];
-//     }
-// }else{
-//     $cmd = "なし";
-// }
-
-// if(isset($_POST['変更'])){
-//     $mode = $_COOKIE['mode'];
-//     var_dump($mode);
-//     if(isset($_POST['name'],$_POST['no'],$_POST['password'],$_POST['authority']) && $mode == "名前"){
-//         $name = $_POST['name'];
-//         $no = $_POST['no'];
-//         $password = $_POST['password'];
-//         $authority = $_POST['authority'];
-//         $word = $_COOKIE['word'];
-//         $sql = "update management.teacher set 名前 = ?,教員番号 = ?,
-//                 パスワード = ?,権限 = ? where 名前 = ?";
-//     }
-//     else if(isset($_POST['name'],$_POST['no'],$_POST['password'],$_POST['authority']) && $mode == "教員番号"){
-//         $name = $_POST['name'];
-//         $no = $_POST['no'];
-//         $password = $_POST['password'];
-//         $authority = $_POST['authority'];
-//         $word = $_COOKIE['word'];
-//         var_dump($mode);
-//         $sql = "update management.teacher set 名前 = ?,教員番号 = ?,
-//                 パスワード = ?,権限 = ? where 教員番号 = ?";
-//     }
-//     echo $sql;
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->execute(array($name,$no,$password,$authority,$word));
-//     echo "できた";
-//     }
-    
-    //$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
--->
+  if(isset($_POST['検索'])){
+    if(isset($_POST['word']) && $_POST['mode'] == "名前"){
+        $mode = "名前";
+        $word = $_POST['word'];
+        setcookie("word",$_POST['word']);
+        setcookie("mode",$_POST['mode']);
+        $sql = "select * from management.student where 名前 = ?";
+        var_dump($sql);
+    }else if(isset($_POST['word']) && $_POST['mode'] == "学籍番号"){
+        $mode = "学籍番号";
+        $word = $_POST['word'];
+        setcookie("word",$_POST['word']);
+        setcookie("mode",$_POST['mode']);
+        $sql = "select * from management.student where 学籍番号 = ?";
+        var_dump($sql);
+    }else{
+        header("Location:{$gobackURL}");
+    }
+if(isset($_POST['変更'])){
+    $mode = $_COOKIE['mode'];
+    var_dump($mode);
+    if(isset($_POST['name'],$_POST['no'],$_POST['password'],$_POST['authority']) && $mode == "名前"){
+        $name = $_POST['name'];
+        $no = $_POST['no'];
+        $password = $_POST['password'];
+        $authority = $_POST['authority'];
+        $word = $_COOKIE['word'];
+        $sql = "update management.teacher set 名前 = ?,教員番号 = ?,
+                パスワード = ?,権限 = ? where 名前 = ?";
+    }
+    else if(isset($_POST['name'],$_POST['no'],$_POST['password'],$_POST['authority']) && $mode == "教員番号"){
+        $name = $_POST['name'];
+        $no = $_POST['no'];
+        $password = $_POST['password'];
+        $authority = $_POST['authority'];
+        $word = $_COOKIE['word'];
+        var_dump($mode);
+        $sql = "update management.teacher set 名前 = ?,教員番号 = ?,
+                パスワード = ?,権限 = ? where 教員番号 = ?";
+    }
+    echo $sql;
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($name,$no,$password,$authority,$word));
+    echo "できた";
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="jp">
 <head>
     <meta charset="UTF-8">
-    <link href="contents.css" rel="stylesheet" media="all">
+    <!-- <link href="contents.css" rel="stylesheet" media="all"> -->
+    <link href="test.css" rel="stylesheet" media="all">
     <title>生徒情報変更</title>
 </head>
 <body>
@@ -117,34 +95,36 @@
     <input id="button" type="submit" value="検索" name="検索"><br>
 </form><br>
 <!--入力フォーム-->
-<form id="formmain" action="" method="post" >
+<form action="" method="post" >
+<div  id="formmain">
+<dl>
     <!--名前-->
-    <span class="font1">*必須</span>　お名前　
-    <input id="input" type="text" value="<?=$name?>"name="name" required ><br>
+    <dt><span style="color:red;">*必須 </span><label for="name">お名前<label></dt>
+    <dd><input id="name" type="text" name="name" value="<?=$name?>" required></dd>
     <!-- フリガナ -->
-    <span class="font1">*必須</span>　フリガナ
-    <input id="input" type="text" name="" value=""　required placeholder="例：ヤマダタロウ"><br>
-    <!--学籍番号-->　
-    <span class="font1">*必須</span>　学籍番号
-    <input id="input" type="text" name="no" value="<?=$no?>"　required placeholder="例：x00n000"><br>
+    <dt><span style="color:red;">*必須 </span><label for="name2">フリガナ<label></dt>
+    <dd><input id="name2" type="text" name="name2" value="<?=$name2?>" required placeholder="例：サトウタロウ"></dd>
+    <!--学籍番号-->
+    <dt><span style="color:red;">*必須 </span><label for="no">学籍番号<label></dt>
+    <dd><input id="no" type="text" name="no" value="<?=$no?>" required placeholder="例：x00n000"></dd>
     <!-- 学年 -->
-    <span class="font1">*必須</span>　学年
-    <input id="input" type="text" name="" value=""　required><br>
+    <dt><span style="color:red;">*必須 </span><label for="class">学年<label></dt>
+    <dd><input id="class" type="text" name="class" value="<?=$class?>"　required></dd>
     <!-- クラス -->
-    <span class="font1">*必須</span>　クラス
-    <input id="input" type="text" name="" value=""　required><br>
+    <dt><span style="color:red;">*必須 </span><label for="class2">クラス<label></dt>
+    <dd><input id="class2" type="text" name="class2" value="<?=$class2?>"　required></dd>
     <!--パスワード-->
-    <span class="font1">*必須</span>　パスワード
-    <input id="input" type="password" name="password" required placeholder="例：abedefg"><br>
+    <dt><span style="color:red;">*必須 </span><label for="password">パスワード<label></dt>
+    <dd><input id="password" type="password" name="password" value="<?=$password?>" required placeholder="例：abedefg"></dd>
     <!--メアド-->
-    <span class="font1">*必須</span>　メールアドレス　
-    <input id="input" type="email" name="mail" required placeholder="例：Example@xxx.com"><br>
+    <dt><span style="color:red;">*必須 </span><label for="mail">メールアドレス<label></dt>
+    <dd><input id="mail" type="email" name="mail" value="<?=$mail?>" required placeholder="例：Example@xxx.com"></dd>
     <!--電話番号-->
-    <span class="font1">*必須</span>　電話番号　　　　
-    <input id="input" type="tel" name="tel" required placeholder="ハイフンなし"><br>
+    <dt><span style="color:red;">*必須 </span><label for="tel">電話番号<label></dt>
+    <dd><input id="tel" type="tel" name="tel" value="<?=$tel?>" required placeholder="ハイフンなし"></dd>
     <!--学科-->
-    <span class="font1">*必須</span>　学科　　　　　　
-    <select id="input" name="subject" required>
+    <dt><label for="subject">学科<label></dt>
+    <dd><select id="subject" name="subject" required>
         <option value="" selected>学科を選択し直してください</option>
         <option value="0">ITエンジニア科4年制</option>
         <option value="1">ITエンジニア化3年制</option>
@@ -152,9 +132,9 @@
         <option value="3">情報ネットワーク科</option>
         <option value="4">WEBクリエーター科</option>
         <option value="5">こども学科</option>
-    </select><br>
-    使用路線(1)　　　
-    <select id="input" name="train1" >
+    </select></dd>
+    <dt><label for="train1">使用路線<label></dt>
+    <dd><select id="train1" name="train1" >
         <option value="" selected>使用する路線(1路線目)を選んでください</option>
         <option value="京成本線1">京成本線</option>
         <option value="京成千葉線">京成千葉線</option>
@@ -166,9 +146,9 @@
         <option value="内房線">内房線</option>
         <option value="常磐線">常磐線</option>
         <option value="常総線">常総線</option>
-    </select><br>
-    使用路線(2)　　　　
-    <select id="input" name="train2">
+    </select></dd>
+    <dt><label for="train2">使用路線2<label></dt>
+    <dd><select id="train2" name="train2">
         <option value="" selected>使用する路線(2路線目)を選んでください</option>
         <option value="京成本線-2">京成本線</option>
         <option value="京成千葉線-2">京成千葉線</option>
@@ -180,9 +160,9 @@
         <option value="内房線-2">内房線</option>
         <option value="常磐線-2">常磐線</option>
         <option value="常総線-2">常総線</option>
-    </select><br>
-    使用路線(3)　　　　
-    <select id="input" name="train3">
+    </select></dd>
+    <dt><label for="train3">使用路線3<label></dt>
+    <dd><select id="train3" name="train3">
         <option value="" selected>使用する路線(3路線目)を選んでください</option>
         <option value="京成本線-3">京成本線</option>
         <option value="京成千葉線-3">京成千葉線</option>
@@ -194,8 +174,10 @@
         <option value="内房線-3">内房線</option>
         <option value="常磐線-3">常磐線</option>
         <option value="常総線-3">常総線</option>
-    </select><br>
+    </select></dd>
+</dl>
     <input id="button" type="submit" value="変更" name="変更"onclick="return checkupdate()">
+</div>
 </form>
 <script>
     function checkupdate(){
