efc60499142d67448fbd42c402bc1d0fc2b7b5b4
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-12-03 02:36:39 +0900

    bootstrap4使用中

--- Web/student_update.php
+++ Web/student_update.php
@@ -74,8 +74,9 @@ if(isset($_POST['変更'])){
 <html lang="jp">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- <link href="contents.css" rel="stylesheet" media="all"> -->
-    <link href="test.css" rel="stylesheet" media="all">
+    <!-- <link href="test.css" rel="stylesheet" media="all"> -->
     <title>生徒情報変更</title>
 </head>
 <body>
@@ -96,18 +97,28 @@ if(isset($_POST['変更'])){
     <input id="button" type="submit" value="検索" name="検索"><br>
 </form><br>
 <!--入力フォーム-->
-<form action="" method="post" >
-<div  id="formmain">
-<dl>
+<form action="" method="post">
     <!--名前-->
-    <dt><span style="color:red;">*必須 </span><label for="name">お名前<label></dt>
-    <dd><input id="name" type="text" name="name" value="<?=$name?>" required></dd>
+    <div class="form-group row">
+    <label class="col-sm-2 control-label"><span style="color:red;">*必須 </span>お名前</label>
+    <div class="col-sm-10">
+        <input class="form-control" type="text" name="name" value="<?=$name?>" required>
+    </div>
+</div>
     <!-- フリガナ -->
-    <dt><span style="color:red;">*必須 </span><label for="name2">フリガナ<label></dt>
-    <dd><input id="name2" type="text" name="name2" value="<?=$name2?>" required placeholder="例：サトウタロウ"></dd>
+<div class="form-group row">
+    <label class="col-sm-2 control-label"><span style="color:red;">*必須 </span>フリガナ</label>
+    <div class="col-sm-10">
+        <input class="form-control" type="text" name="name2" value="<?=$name2?>" required placeholder="例：サトウタロウ">
+    </div>
+</div>
     <!--学籍番号-->
-    <dt><span style="color:red;">*必須 </span><label for="no">学籍番号<label></dt>
-    <dd><input id="no" type="text" name="no" value="<?=$no?>" required placeholder="例：x00n000"></dd>
+<div class="form-group row">
+    <label class="col-sm-2 control-label"><span style="color:red;">*必須 </span>学籍番号</label>
+    <div class="col-sm-10">
+        <input class="form-control" type="text" name="no" value="<?=$no?>" required placeholder="例：x00n000">
+    </div>
+</div>
     <!-- 学年 -->
     <dt><span style="color:red;">*必須 </span><label for="class">学年<label></dt>
     <dd><input id="class" type="text" name="class" value="<?=$class?>"　required></dd>
@@ -187,5 +198,8 @@ if(isset($_POST['変更'])){
 </script>
 <!--copyright-->
 <footer>copyright© チームコリジョン</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
