840424f62c2ed55b8c03b401352676c0cbd69f91
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-12-04 07:19:36 +0900

    処理部分書いています

--- Web/main.php
+++ Web/main.php
@@ -29,44 +29,88 @@ $level = $_SESSION['権限'];
 <!-- ログアウトボタン 動きはわからん -->
 <button type=“button” id="button" onclick="location.href='logout.php'">ログアウト</button>
 <H1>本日の出席状況</H1><br>
-    <?php
+<?php
 //sotukenサーバー用のDB情報
 //require_once('main_config.php');
 //ローカル用のサーバー情報
 require_once 'localhost_config.php';
 
 try{
-  $dbh = new PDO(DSN, DB_USER, DB_PASS);
-  $timestamp = new DataTime();
-  $timestamp = $timestamp->format('Y-m-d');
-  $sql = 'SELECT student.学籍番号,student.名前,student.クラス,attend.登校時間,attend.登校日,attend.備考 FROM  management.student
-          left outer join management.attend
-          on student.学籍番号 = attend.学籍番号
-          WHERE attend.登校日 like '%11-2%'';
-?>
+  $pdo = new PDO(DSN, DB_USER, DB_PASS);
+  // タイムゾーンを日本に設定
+  date_default_timezone_set('Asia/Tokyo');
+  // 現在時刻の取得
+  $time = new DateTime();
+  $time = $time->format('Y-m-d');
+  // クエリ
+  $sql = 'SELECT student.学籍番号,student.名前,student.学年,student.クラス,attend.登校時間,attend.登校日,attend.備考 FROM  management.student
+  left outer join management.attend
+  on student.学籍番号 = attend.学籍番号
+WHERE attend.登校日 = ?';
+  // SQL文の実効
+  $stm = $pdo->prepare($sql);
+  $stm->execute(array($time));
+// 結果の取得（連想配列で受け取る）
+$result = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-<body>
-    <div id='style table'>
-        <table border="1">
-        <tr>
+  ?>
+<div id='style table'>
+<table border="1">
+  <tr>
   <th>学籍番号</th>
   <th>名前</th>
   <th>クラス</th>
   <th>出席状況</th>
   <th>登校時間</th>
-  <th>備考</th>
+  <th>コメント</th>
   </tr>
+
   <?php
-  foreach ($dbh->query($sql) as $row) { ?>
+  
+  foreach ($result as $row) { 
+  $row['クラス'] = $row['学年']."-".$row['クラス']; 
+  if(!empty($row['登校時間'])){
+    if($row['登校時間']);
+  }
+  
+  ?>
     <tr>
     <td><?php print($row['学籍番号']);?>
     <td><?php print($row['名前']);?>
-    <td><?php print($row['パスワード']);?>
-    <td><?php print($row['権限']);?>
+    <td><?php print($row['クラス']);?>
+    <td>
+    <td><?php print($row['登校時間']);?>
+    <td><?php print($row['備考']);?>
     </tr>
       <?php
   }
     ?>
     </table>
+    <div class="float-sample-4">
+      <p>　　　　　</p>
+    </div>
+<!--リスト黒四角つけるタグ-->
+<ul style="list-style-type: disc">
+<!--教員登録リンク-->
+<?php if($level == 0){
+echo '<li><a href="teacher_insert.php">教員登録</li><br>';
+}?>
+<!--教員情報変更リンク-->
+<?php if($level == 0){
+echo '<li><a href="teacher_update.php">教員情報変更</li><br>';
+}?>
+<!--教員削除リンク-->
+<?php if($level == 0){
+echo '<li><a href="teacher_delete.php">教員削除</li><br>';
+}?>
+</ul>
+    <?php
+}catch (PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
+$dbh = null;
+?>
+<footer>copyright© チームコリジョン</footer>
 </body>
 </html>
