2882e212ce0acae4db4994c00d343cd14fa3ebbd
takahashi-0124 <x18n050@chiba.ac.jp>
2019-12-03 10:53:48 +0900

    56,58,60に/a入れてみた

--- Web/student_list(練習用).php
+++ Web/student_list(練習用).php
@@ -53,11 +53,11 @@ try{
 <!--リスト黒四角つけるタグ-->
 <ul style="list-style-type: disc">
 <!--教員登録リンク-->
-<li><a href="student_insert.php">生徒登録</li><br>
+<li><a href="student_insert.php">生徒登録</a></li><br>
 <!--教員情報変更リンク-->
-<li><a href="student_update.php">生徒情報変更</li><br>
+<li><a href="student_update.php">生徒情報変更</a></li><br>
 <!--教員削除リンク-->
-<li><a href="student_delete.php">生徒削除</li><br>
+<li><a href="student_delete.php">生徒削除</a></li><br>
 </ul>
     <?php
 }catch (PDOException $e){
