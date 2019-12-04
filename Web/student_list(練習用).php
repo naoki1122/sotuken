c5fd1b7384d2cb621d1e386cdf6a6f49d8d7f61a
takahashi-0124 <x18n050@chiba.ac.jp>
2019-12-04 09:39:10 +0900

    カラムサイズをautoにしてみた

--- Web/student_list(練習用).php
+++ Web/student_list(練習用).php
@@ -24,7 +24,7 @@ try{
   $dbh = new PDO(DSN, DB_USER, DB_PASS);
   $sql = 'select * from student';
 ?>
-<div class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content float=left">
+<div class="col-auto py-md-3 pl-md-5 bd-content float=left">
 <table class="table table-striped table-bordered">
 <thead class="thead-dark">
   <tr>
