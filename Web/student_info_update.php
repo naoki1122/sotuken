e6053406ec09f06813087f417d2f1aada2f18c3f
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-12-05 12:42:47 +0900

    student_info_update.php 変更ボタンを押したときの処理がまだ

--- Web/student_info_update.php
+++ Web/student_info_update.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+require_once('server_config.php');
+try{
+  $pdo = new PDO(DSN, DB_USER, DB_PASS);
+  $day="";
+  $word = $_SESSION['word'];
+  if(isset($_POST['検索'])){
+    if(isset($_POST['day'])){
+    $day = $_POST['day'];
+    }else{
+    $day="";
+  }
+}
+  $sql = 'select * from management.attend where 学籍番号 = ? and 登校日 = ?';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(array($word,$day));
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach($result as $row){
+      $day = $row['登校日'];
+      $attend = $row['登校時間'];
+  }
+  var_dump($word);
+
+  if(isset($_POST['変更'])){
+      if(isset($_POST['attend'],$_POST['day'])){
+        $sql = "update management.attend set  登校日 = ?,登校時間 = ?,
+                学籍番号 = ?";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute(array($day,$attend,$word));
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                }
+            }
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+  }
+  $pdo = null;
+?>
 <!DOCTYPE html>
 <html lang="jp">
 <head>
@@ -12,16 +52,16 @@
 <!--検索フォーム-->
 <form id ="search" action="" method="post">
     <!--検索条件入力-->　　　
-    <input id="input" type="text" name="name" required autofocus placeholder="日付の入力"><br>
+    <input id="input" type="text" name="day" required autofocus placeholder="日付の入力"><br>
     <!--検索ボタン-->
     <input id="button" type="submit" value="検索" name="検索"><br>
 </form><br>
 <!--入力フォーム-->
 <form id="formmain" action="" method="post" >
     <!--名前-->日付
-    <input id="input" type="text" readonly value="<?=$name?>" name="name" required><br>
+    <input id="input" type="text" readonly value="<?=$day?>" name="day"><br>
     <!--教員番号-->出席時刻
-    <input id="input" type="text" readonly value="<?=$no?>" name="no" required><br>
+    <input id="input" type="text"  value="<?=$attend?>" name="attend" ><br>
     <input id="button" type="submit" value="変更" name="変更"onclick="return checkdelete()">
 </form>
 <!--copyright-->
