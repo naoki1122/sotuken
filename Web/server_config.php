f4b819f9af880b498b8761c1058c94baaeb71446
hakozaki1104 <syun-418@ezzwed.ne.jp>
2019-12-05 11:27:43 +0900

    内容をローカルに変更

--- Web/server_config.php
+++ Web/server_config.php
@@ -2,6 +2,10 @@
 
 //ini_set('display_errors', 1);
 
-define('DSN', 'mysql:host=192.168.1.2;dbname=management');
-define('DB_USER', 'user');
-define('DB_PASS', 'marioff3');
+// define('DSN', 'mysql:host=192.168.1.2;dbname=management');
+// define('DB_USER', 'user');
+// define('DB_PASS', 'marioff3');
+
+const DSN = "mysql:host=localhost;dbname=management";
+const DB_USER = "root";
+const DB_PASS = "";
