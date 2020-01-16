43f32710c8c2bba344eaea042e06c43fe227321d
takahashi-0124 <x18n050@chiba.ac.jp>
2020-01-16 15:53:09 +0900

    </a>追加

--- Web/main.php
+++ Web/main.php
@@ -128,15 +128,15 @@ $result = $stm->fetchAll(PDO::FETCH_ASSOC);
 <ul style="list-style-type: disc">
 <!--学生管理リンク-->
 <?php if($level == 0){
-echo '<li><a href="student_list.php">学生管理</li></a><br>';
+echo '<li><a href="student_list.php">学生管理</a></li><br>';
 }?>
 <!--教員管理リンク-->
 <?php if($level == 0){
-echo '<li><a href="teacher_list.php">教員管理</li></a><br>';
+echo '<li><a href="teacher_list.php">教員管理</a></li><br>';
 }?>
 <!--CSV出力リンク-->
 <?php if($level == 0){
- echo '<li><a href="csv.php">CSV出力</li></a><br>';
+ echo '<li><a href="csv.php">CSV出力</a></li><br>';
 }?>
 </ul>
     <?php
