6f1b73aaa86dd83eb5a10a065e8e80512f955909
takahashi-0124 <x18n050@chiba.ac.jp>
2020-01-16 16:16:11 +0900

    ログアウトボタン追加

--- Web/student_info_update.php
+++ Web/student_info_update.php
@@ -48,6 +48,11 @@ try{
 <body>
 <!--戻るのリンク-->
 <a href="student_info.php">戻る</a><br>
+<p> </p><br>
+<!-- ようこそ的なメッセージ 名前抽出わからん-->
+<p>ようこそ　ゲストさん</p>
+<!-- ログアウトボタン 動きはわからん -->
+<button type=“button” id="button" onclick="location.href='logout.php'">ログアウト</button>
 <H1>生徒出席情報変更</H1><br>
 <!--検索フォーム-->
 <form id ="search" action="" method="post">
