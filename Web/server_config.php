31a4c286fe77b27035a90936e8f2478e866b4260
hakozaki1104 <syun-418@ezzwed.ne.jp>
2020-01-22 04:01:51 +0900

    卒研サーバーのconfigをコメントで追加

--- Web/server_config.php
+++ Web/server_config.php
@@ -2,9 +2,9 @@
 
 //ini_set('display_errors', 1);
 
-// define('DSN', 'mysql:host=192.168.1.2;dbname=management');
-// define('DB_USER', 'user');
-// define('DB_PASS', 'marioff3');
+// const DSN = 'DSN', 'mysql:host=192.168.1.2;dbname=management';
+// const DB_USER = 'DB_USER', 'user';
+// const DB_PASS = 'DB_PASS', 'marioff3';
 
 const DSN = "mysql:host=localhost;dbname=management";
 const DB_USER = "root";
