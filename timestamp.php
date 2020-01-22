15f8b8c1269f0ac60a24f20252188f7a0811e732
hakozaki1104 <syun-418@ezzwed.ne.jp>
2020-01-22 11:12:04 +0900

    曜日のみの取得を追加

--- timestamp.php
+++ timestamp.php
@@ -3,7 +3,12 @@
 	$timestamp = time() ;
 	// 曜日の配列
 	$weekday = array( '日' , '月' , '火' , '水' , '木' , '金' , '土' ) ;
- 
+	var_dump($timestamp);
 	// date()で日時を出力
 	echo date( "Y年m月j日 " .$weekday[ date('w') ]."曜日 A H時i分s秒" , $timestamp ) ;
+
+	$week = array("日", "月", "火", "水", "木", "金", "土");
+$datetime = date_create();
+$w = (int)date_format($datetime, 'w');
+echo $week[$w];
 ?>
