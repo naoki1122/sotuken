bdcaf149ad4976f6de101b3394737538bd3157c9
hakozaki1104 <syun-418@ezzwed.ne.jp>
2020-01-23 12:42:36 +0900

    動作確認済み 権限の日本語変換終わってないです

--- Web/use_file/teacher_delete.php
+++ Web/use_file/teacher_delete.php
@@ -3,11 +3,11 @@ session_start();
 //sotukenサーバー用のDB情報
 require_once "server_config.php";
 require_once "lib.php";
+
 $pdo = dbcon();
 
 $gobackURL="teacher_list.php";
 $tbl="management.teacher";
-
 
 if(empty($_POST['NAME'])) $name = null;
 if(empty($_POST['T_NO'])) $t_no = null;
@@ -22,45 +22,56 @@ if(empty($_SESSION['名前'])&&empty($_SESSION['権限'])){
   $session_level = $_SESSION['権限'];
   }
 
-  if(isset($_POST['word'])) $word = $_POST['word'];
+  
 
   if(isset($_POST['検索'])){
+    if(isset($_POST['word'])) $word = $_POST['word'];
     if(isset($_POST['word']) && $_POST['mode'] == "名前"){
-    $sql = "select * from management.teacher where 名前 = ?";
+    $sql = "select * from ${tbl} where 名前 = :word";
         }else if(isset($_POST['word']) && $_POST['mode'] == "教員番号"){
-            $sql = "select * from management.teacher where 教員番号 = ?";
+            $sql = "select * from ${tbl} where 教員番号 = :word";
     }
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$word]);
+    $stmt->bindValue(":word", $word, PDO::PARAM_STR);
+    $stmt->execute();
+    var_dump($stmt);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($result as $row){
        $name  = $row["名前"];
-       $no = $row["教員番号"];
-       $password = $row["パスワード"];
+       $t_no = $row["教員番号"];
+       $pass = $row["パスワード"];
        $authority = $row["権限"];
     }
 }else{
     $cmd = "なし";
 }
     if(isset($_POST['削除'])){
-        $name = $_POST['name'];
-        $no = $_POST['no'];
-        $password = $_POST['password'];
-        $authority = $_POST['authority'];
-        $sql = "delete from management.teacher where 名前 = ? and 教員番号 = ? and パスワード = ? and 権限 = ?";
+        $name = $_POST['NAME'];
+        $t_no = $_POST['T_NO'];
+        $pass = $_POST['PASSWD'];
+        $authority = $_POST['AUTHORITY'];
+        $sql = "delete from ${tbl} where 名前 = :name and 教員番号 = :t_no and パスワード = :pass and 権限 = :authority";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(array($name,$no,$password,$authority));
-    echo "できた";
+    $stmt->bindValue(":name", $name, PDO::PARAM_STR);
+    $stmt->bindValue(":t_no", $t_no, PDO::PARAM_STR);
+    $stmt->bindValue(":pass", $pass, PDO::PARAM_STR);
+    $stmt->bindValue(":authority", $authority, PDO::PARAM_STR);
+    $stmt->execute();
+    $name = "";
+    $t_no = "";
+    $pass = "";
+    $authority = "";
     }
     
-    //$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//出来てない！！！権限を数値から名前に変えるやつ
+
 // if($authority == 0){
-//     $rename = str_replace('0','管理者',$authority)
-//         }elseif($authority == 1){
-//             $rename = str_replace('1','教員',$authority)
-//                 }else{
-//                     $rename = str_replace('2','アシスタント',$authority)
+//     $rename = "管理者";
+// }elseif($authority == 1){
+//     $rename ="教員";
+// }elseif($authority == 2){
+//     $rename ="アシスタント";
+// }else{
+//     $rename = "";
 // }
 ?>
 
@@ -77,8 +88,6 @@ if(empty($_SESSION['名前'])&&empty($_SESSION['権限'])){
 <a href="teacher_list.php">戻る</a><br>
 <!-- ログイン中の名前 -->
 <p>ようこそ<?=$session_name?>さん</p>
-<!-- コメント -->
-<p id="cmd"><?=$cmd?></p>
 <!-- ログアウトボタン -->
 <button type=“button” id="button" onclick="location.href='logout.php'">ログアウト</button>
 <h1>教員削除</h1>
@@ -103,13 +112,13 @@ if(empty($_SESSION['名前'])&&empty($_SESSION['権限'])){
     <section id="input_form">
 <ul>
     <!--名前-->
-    <li><lavel><span class="item">お名前</span><input class="inputbox" type="text" readonly value="<?=$name?>" name="name" required></lavel></li>
+    <li><lavel><span class="item">お名前</span><input class="inputbox" type="text" readonly value="<?=$name?>" name="NAME" required></lavel></li>
     <!--教員番号-->
-    <li><lavel><span class="item">教員番号</span><input class="inputbox" type="text" readonly value="<?=$no?>" name="no" required></lavel></li>
+    <li><lavel><span class="item">教員番号</span><input class="inputbox" type="text" readonly value="<?=$t_no?>" name="T_NO" required></lavel></li>
     <!--パスワード-->
-    <li><lavel><span class="item">パスワード</span><input class="inputbox" type="password" readonly value="<?=$password?>" name="password"></lavel></li>
+    <li><lavel><span class="item">パスワード</span><input class="inputbox" type="password" readonly value="<?=$pass?>" name="PASSWD"></lavel></li>
     <!--権限-->
-    <li><lavel><span class="item">権限</span><input class="inputbox" type="text" readonly value="<?=$authority?>" name="authority"></lavel></li>
+    <li><lavel><span class="item">権限</span><input class="inputbox" type="text" readonly value="<?=$authority?>" name="AUTHORITY"></lavel></li>
 </ul>
 <input id="button" type="submit" value="削除" name="削除" onclick="return checkupdate()">
     </section>
