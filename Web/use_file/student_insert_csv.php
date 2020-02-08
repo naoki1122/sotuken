46c0bdc07838c4e30e03e91e3d16096d76c46a67
hakozaki1104 <syun-418@ezzwed.ne.jp>
2020-02-08 23:47:11 +0900

    表示の機能は完成

--- Web/use_file/student_insert_csv.php
+++ Web/use_file/student_insert_csv.php
@@ -1,15 +1,31 @@
 <?php
 session_start();
 require_once 'server_config.php';
+require_once "lib.php";
+
 $gobackURL = "teacher_signup.html";
 if(empty($_SESSION['名前'])&&empty($_SESSION['権限'])){
   header("Location:{$gobackURL}");
 }else{
-$name = $_SESSION['名前'];
-$level = $_SESSION['権限'];
+  $session_name = $_SESSION['名前'];
+  $session_level = $_SESSION['権限'];
 }
-$csv;
-if($_POST['表示']){
+
+if(isset($_POST['NAME_UP']))$name_up = $_POST['NAME_UP'];
+if(isset($_POST['NAME_DOWN']))$name_down = $_POST['NAME_DOWN'];
+if(isset($_POST['HURI']))$huri = $_POST['HURI'];
+if(isset($_POST['S_NO']))$s_no = $_POST['S_NO'];
+if(isset($_POST['PASSWD']))$pass = $_POST['PASSWD'];
+if(isset($_POST['YEAR']))$year = $_POST['YEAR'];
+if(isset($_POST['CLASS']))$class = $_POST['CLASS'];
+if(isset($_POST['SUBJECT']))$subject = $_POST['SUBJECT'];
+if(isset($_POST['MAIL']))$mail = $_POST['MAIL'];
+if(isset($_POST['TEL']))$tel = $_POST['TEL'];
+if(isset($_POST['TRAIN1']))$train1 = $_POST['TRAIN1'];
+if(isset($_POST['TRAIN2']))$train2 = $_POST['TRAIN2'];
+if(isset($_POST['TRAIN3']))$train3 = $_POST['TRAIN3'];
+
+if(isset($_POST['表示'])){
 setlocale(LC_ALL, 'ja_JP.UTF-8');
 
 $file = 'csv_inport.csv';
@@ -26,7 +42,62 @@ while (($data = fgetcsv($temp, 0, ",")) !== FALSE) {
 }
 fclose($temp);
 
-var_dump($csv);
+}
+
+if(isset($_POST['登録'])){
+  setlocale(LC_ALL, 'ja_JP.UTF-8');
+
+$file = 'csv_inport.csv';
+$data = file_get_contents($file);
+$data = mb_convert_encoding($data, 'UTF-8', 'auto');
+$temp = tmpfile();
+$csv  = array();
+
+fwrite($temp, $data);
+rewind($temp);
+
+while (($data = fgetcsv($temp, 0, ",")) !== FALSE) {
+    $csv[] = $data;
+}
+fclose($temp);
+
+  $tbl ="management.student";
+  $pdo = dbcon();
+  $sql = "INSERT INTO ${tbl}(学籍番号,学年,クラス,学科,名前,フリガナ,メールアドレス,電話番号,路線1,路線2,路線3)
+          VALUES (:s_no,:year,:class,:subject,:name,:huri,:mail,:tel,:train1,:train2,:train3)";
+  $stmt = $pdo->prepare($sql);
+  if(!empty($s_no))$stmt->bindValue(":s_no", $s_no, PDO::PARAM_STR);
+  if(!empty($year))$stmt->bindValue(":year", $year, PDO::PARAM_INT);
+  if(!empty($class))$stmt->bindValue(":class", $class, PDO::PARAM_INT);
+  if(!empty($subject))$stmt->bindValue(":subject", $subject, PDO::PARAM_STR);
+  if(!empty($name))$stmt->bindValue(":name", $name, PDO::PARAM_STR);
+  if(!empty($huri))$stmt->bindValue(":huri", $huri, PDO::PARAM_STR);
+  if(!empty($mail))$stmt->bindValue(":mail", $mail, PDO::PARAM_STR);
+  if(!empty($tel))$stmt->bindValue(":tel", $tel, PDO::PARAM_STR);
+  if(!empty($train1))$stmt->bindValue(":train1", $train1, PDO::PARAM_STR);
+  if(!empty($train2))$stmt->bindValue(":train2", $train2, PDO::PARAM_STR);
+  if(!empty($train3))$stmt->bindValue(":train3", $train3, PDO::PARAM_STR);
+
+  foreach ($csv as $row) { 
+    
+     foreach($row as $v){
+    $name =$v;
+    $huri =$v;
+    $s_no =$v;
+    $pass =$v;
+    $year =$v;
+    $class =$v;
+    $subject =$v;
+    $mail =$v;
+    $tel =$v;
+    $train1 =$v;
+    $train2 =$v;
+    $train3 =$v;
+    $stmt->execute();
+   }
+
+  }
+  $pdo = null;
 }
 ?>
 
@@ -43,7 +114,7 @@ var_dump($csv);
 <button type=“button” id="back-button" onclick="location.href='student_list.php'">戻る</button><br>
 <p> </p><br>
 <!-- ログイン中の名前 -->
-<p>ようこそ<?=$name?>さん</p>
+<p>ようこそ<?=$session_name?>さん</p>
 <!-- ログアウトボタン -->
 <button type=“button” id="logout-button" onclick="location.href='logout.php'">ログアウト</button>
 <H1>CSV生徒取り込み</H1><br>
@@ -51,15 +122,15 @@ var_dump($csv);
 <form action="" method="post" enctype="multipart/form-data">
 <input type="file" name="filename" accept=".csv">
 <input id="button" type="submit" value="表示" name="表示" >
-<input id="button" type="submit" value="登録" >
+<input id="button" type="submit" value="登録" name="登録">
 </form><br>
     <?php
-try{
-  $dbh = new PDO(DSN, DB_USER, DB_PASS);
-  $sql = 'select * from student';
+
+  
 ?>
 <!-- <input id="input1" type="text" value size="100"> -->
 <div class='scroll-table-csv'>
+<?php if(!empty($csv)){?>
 <table id="sample1" border="1">
 <thead>
   <tr>
@@ -89,21 +160,13 @@ try{
   }
   
   echo "</tbody>";
+}
     ?>
     </table>
 </div>
 <div class="float-sample-4">
       <p>　　　　　</p>
 </div>
-    <?php
-}catch (PDOException $e){
-  print('Error:'.$e->getMessage());
-  die();
-}
-$dbh = null;
-
-var_dump($_FILES);
-?>
 <footer>copyright© チームコリジョン</footer>
 </body>
 </html>
